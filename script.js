639978133e747da5715ecdef0bf7f1e130b06357
Muzahid <mdmuzahid.gd@gmail.com>
2023-05-26 20:00:18 +0600

    calculator part updated with digit visibility

--- script.js
+++ script.js
@@ -8,4 +8,33 @@ document.getElementById("Generate").addEventListener("click", function(){
         const PinField = document.getElementById("PinField");
         PinField.value = Pin;
     }
+});
+
+document.getElementById("CalculatorButtons").addEventListener("click", function(event){
+    const Digit = event.target.innerText;
+    const PinSubmitField = document.getElementById("PinSubmit");
+    const PreviousDigit = PinSubmit.value;
+    if(isNaN(Digit)){
+        // console.log("Character string")
+        // C means Clear the all submitted digits
+        if(Digit == "C"){
+            PinSubmitField.value = ""
+        }
+        else{
+            const allDigits = PreviousDigit.split("");
+            allDigits.pop();
+            // console.log(allDigits) remove one by one digit for per click
+            // add all digits in an array without removed digits
+            const NewDigits = allDigits.join("");
+            PinSubmitField.value = NewDigits;
+        }   
+    }
+    else{
+        // console.log("Number String")
+        // const PinSubmitField = document.getElementById("PinSubmit");
+        // const PreviousDigit = PinSubmit.value;
+        const NextDigit = Digit;
+        const TotalPin = PreviousDigit + NextDigit;
+        PinSubmitField.value = TotalPin;
+    }
 })
